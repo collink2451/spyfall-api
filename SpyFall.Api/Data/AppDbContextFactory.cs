463b98ca2bdcf23783172d6e92500e743005ac88
Collin Koldoff <collinkoldoff@gmail.com>
2026-04-25 13:20:54 -0500

    Add design-time DbContext factory for migrations bundle

---
+++ SpyFall.Api/Data/AppDbContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SpyFall.Api.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseMySql("server=localhost;database=spyfall;user=root;password=placeholder",
+                new MySqlServerVersion(new Version(8, 0, 0)))
+            .Build();
+
+        return new AppDbContext(options);
+    }
+}
